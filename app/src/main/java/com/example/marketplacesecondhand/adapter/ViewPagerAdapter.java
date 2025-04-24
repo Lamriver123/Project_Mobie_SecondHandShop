c14e0ef9c51b20ff2efc716799f915cc8b7a5da1
Lamriver123 <ngoclamhttv@gmail.com>
2025-04-24 16:28:31 +0700

    profile

--- app/src/main/java/com/example/marketplacesecondhand/adapter/ViewPagerAdapter.java
+++ app/src/main/java/com/example/marketplacesecondhand/adapter/ViewPagerAdapter.java
@@ -5,11 +5,13 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 
+import com.example.marketplacesecondhand.fragment.EditProfileFragment;
 import com.example.marketplacesecondhand.fragment.HomeFragment;
 import com.example.marketplacesecondhand.fragment.OrderFragment;
 import com.example.marketplacesecondhand.fragment.ProfileFragment;
 import com.example.marketplacesecondhand.fragment.SearchFragment;
 import com.example.marketplacesecondhand.fragment.StoreFragment;
+import com.example.marketplacesecondhand.fragment.TermsConditionsFragment;
 
 public class ViewPagerAdapter extends FragmentPagerAdapter {
     public ViewPagerAdapter(@NonNull FragmentManager fm) {
@@ -30,6 +32,10 @@ public class ViewPagerAdapter extends FragmentPagerAdapter {
                 return new OrderFragment();
             case 4:
                 return new ProfileFragment();
+//            case 5:
+//                return new EditProfileFragment();
+//            case 6:
+//                return new TermsConditionsFragment();
             default:
                 return new HomeFragment();
         }
---
+++ app/src/main/java/com/example/marketplacesecondhand/fragment/EditProfileFragment.java
@@ -0,0 +1,23 @@
+package com.example.marketplacesecondhand.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.marketplacesecondhand.R;
+
+public class EditProfileFragment extends Fragment {
+    public EditProfileFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_edit_profile, container, false);
+    }
+}
+
