f64cbcce69ac28463d5d5292759d5e8a30d8daf2
Pham Ngoc Duy <duysm17102004@gmail.com>
2025-04-27 01:59:59 +0700

    edit UI login, register, forgot password, reset password

--- app/src/main/java/com/example/marketplacesecondhand/RetrofitClient.java
+++ app/src/main/java/com/example/marketplacesecondhand/RetrofitClient.java
@@ -4,13 +4,13 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-    private static final String BASE_URL_REAL_MACHINE = "http://192.168.100.218:8080/marketplace/";
+    private static final String BASE_URL_REAL_MACHINE = "http://192.168.1.20:8080/marketplace/";
     private static final String BASE_URL_VIRTUAL_MACHINE = "http://10.0.2.2:8080/marketplace/";
     private static Retrofit retrofit;
     public static Retrofit getRetrofit(){
         if(retrofit == null){
             retrofit = new Retrofit.Builder()
-                    .baseUrl(BASE_URL_REAL_MACHINE)
+                    .baseUrl(BASE_URL_VIRTUAL_MACHINE)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
