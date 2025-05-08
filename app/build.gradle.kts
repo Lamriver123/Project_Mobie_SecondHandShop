a7eec20479b9c8288939b8188021f1f5611b8e62
Lamriver123 <ngoclamhttv@gmail.com>
2025-05-08 16:53:50 +0700

    location

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,11 +47,13 @@ dependencies {
     implementation("com.github.bumptech.glide:glide:4.14.2")
     annotationProcessor("com.github.bumptech.glide:compiler:4.14.2")
 
+    implementation ("com.google.android.libraries.places:places:3.4.0")
+
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:3.12.0")
     implementation ("com.google.code.gson:gson:2.10.1")
-    implementation ("com.google.android.material:material:1.10.0") // hoặc phiên bản mới nhất
+    implementation ("com.google.android.material:material:1.11.0") // hoặc phiên bản mới nhất
     implementation("com.google.android.flexbox:flexbox:3.0.0")
 
     implementation ("jp.wasabeef:glide-transformations:4.3.0")
--- app/src/main/java/com/example/marketplacesecondhand/fragment/BodyPaymentFragment.java
+++ app/src/main/java/com/example/marketplacesecondhand/fragment/BodyPaymentFragment.java
@@ -1,5 +1,6 @@
 package com.example.marketplacesecondhand.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,6 +8,7 @@ import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
+import com.example.marketplacesecondhand.databinding.BottomSheetAddLocationBinding;
 import com.example.marketplacesecondhand.databinding.FragmentBodyPaymentBinding;
 
 public class BodyPaymentFragment extends Fragment {
@@ -15,6 +17,11 @@ public class BodyPaymentFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentBodyPaymentBinding.inflate(inflater, container, false);
+
+        binding.btnChooseLocation.setOnClickListener(v -> {
+            BottomSheetAddLocationFragment bottomSheet = new BottomSheetAddLocationFragment();
+            bottomSheet.show(getParentFragmentManager(), bottomSheet.getTag());
+        });
         return binding.getRoot();
     }
 }
